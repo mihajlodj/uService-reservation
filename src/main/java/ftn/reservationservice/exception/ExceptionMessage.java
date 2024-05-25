446d3ae06bcb8f96049230b63b722370d9fbed19
Vuk Vukovic <vuk.vukovic.2000@gmail.com>
2024-05-25 15:24:35 +0200

    Project setup (#2)
    * feat: gradle file and spring security setup
    
    This sets up the gradle file with all project dependencies. It also configures spring security and auth filter
    
    * feat: exception handler & auth utils
    
    This provides custom exceptions and exception controller handler. AuthUtils is implemented which allows jwt information extraction
    
    * feat: application properties setup
    
    * feat: tests configuration
    
    This sets up testcontainers for integration tests

---
+++ src/main/java/ftn/reservationservice/exception/ExceptionMessage.java
@@ -0,0 +1,12 @@
+package ftn.reservationservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionMessage {
+
+    private String message;
+
+}
